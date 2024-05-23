079f3bf8b46ca90cae585bae7cca2e3324c26e72
Sergej Sawazki <sergej.sawazki@mobileye.com>
2024-05-23 17:44:52 +0300

    Add Mobileye S62 board
    Issue: BSWMCU-2210
    Signed-off-by: Sergej Sawazki <sergej.sawazki@mobileye.com>

---
+++ include/configs/me-sv62.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024 Mobileye
+ */
+#ifndef __ME_SV62__
+#define __ME_SV62__
+
+#include <configs/s32g3.h>
+
+#define EXTRA_BOOT_ARGS			""
+#define FDT_FILE			"s32g3xxa-evb3.dtb"
+#endif
